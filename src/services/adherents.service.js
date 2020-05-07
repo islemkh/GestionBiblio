9e44dc0d7b78ab4d5ade57b295690635353dd78e
ESLEMM <islemkhemiri09@gmail.com>
2020-05-08 01:30:34 +0200

    first commit

---
+++ src/services/adherents.service.js
@@ -0,0 +1,30 @@
+const adherents = [
+{
+    id:"1",
+    nom: "islem",
+    prenom: "khemiri",
+    dateNaissance: "23/12/1995"
+},
+{
+    id:"2",
+    nom: "emna",
+    prenom: "amri",
+    dateNaissance: "23/12/1996"
+},
+{
+    id:"3",
+    nom: "maryem",
+    prenom: "souyah",
+    dateNaissance: "12/12/1996"
+},
+{
+    id:"4",
+    nom: "sana",
+    prenom: "kthiri",
+    dateNaissance: "14/06/1996"
+},{
+    id:"5",
+    nom: "ahmed",
+    prenom: "cherif",
+    dateNaissance: "12/03/1996"
+}]
---
+++ src/services/livres.service.js
@@ -0,0 +1,50 @@
+const livres = [
+    {
+        id:"1",
+        titre: "Le Grand Meaulnes",
+        auteur: "Victor Hugo",
+        édition: 2015,
+        NbExemplaires: 20
+      }, 
+      {
+        id:"2",
+        titre: "Le Rouge et le Noir",
+        auteur: "Victor Del Litto ",
+        édition: 1997,
+        NbExemplaires: 5
+      }, 
+      {
+        id:"3",
+        titre: "Le Petit Prince",
+        auteur: "Antoine de Saint-Exupéry",
+        édition: 2014,
+        NbExemplaires: 10
+      }, 
+      {
+        id:"4",
+        titre: "Lettres de mon moulin",
+        auteur: "Alphonse Daudet",
+        édition: 2005,
+        NbExemplaires: 12
+      }, 
+      {
+        id:"5",
+        titre: "La Mère",
+        auteur: "Pearl Buck",
+        édition: 1971,
+        NbExemplaires: 30
+      },
+      {
+        id:"6",
+        titre: "Les Fourmis",
+        auteur: "Bernard Werber",
+        édition: 2002,
+        NbExemplaires: 8
+      },
+       {
+        id:"7",
+        titre: "Si c'est un homme",
+        auteur: "Primo Levi",
+        édition: 2007,
+        NbExemplaires: 6
+      }]
