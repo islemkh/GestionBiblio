f1e184b2ef4d1bd1594f280c0e4a430659cc7136
ESLEMM <islemkhemiri09@gmail.com>
2020-05-08 02:49:27 +0200

    second commit

--- src/services/adherents.service.js
+++ src/services/adherents.service.js
@@ -5,26 +5,31 @@ const adherents = [
     prenom: "khemiri",
     dateNaissance: "23/12/1995"
 },
+
 {
     id:"2",
     nom: "emna",
     prenom: "amri",
     dateNaissance: "23/12/1996"
 },
+
 {
     id:"3",
     nom: "maryem",
     prenom: "souyah",
     dateNaissance: "12/12/1996"
 },
+
 {
     id:"4",
     nom: "sana",
     prenom: "kthiri",
-    dateNaissance: "14/06/1996"
+    dateNaissance: "14/06/1995"
 },{
     id:"5",
     nom: "ahmed",
     prenom: "cherif",
     dateNaissance: "12/03/1996"
 }]
+
+
