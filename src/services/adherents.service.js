1123e8695fc43c11d96de0c3698662a86b66773d
ESLEMM <islemkhemiri09@gmail.com>
2020-05-12 02:25:28 +0200

    add services functions

--- src/services/adherents.service.js
+++ src/services/adherents.service.js
@@ -3,33 +3,59 @@ const adherents = [
     id:"1",
     nom: "islem",
     prenom: "khemiri",
-    dateNaissance: "23/12/1995"
+    dateNaissance: "23/12/1995",
+    statut : "banni"
 },
 
 {
     id:"2",
     nom: "emna",
     prenom: "amri",
-    dateNaissance: "23/12/1996"
+    dateNaissance: "23/12/1996" ,
+    statut : "active"
+
 },
 
 {
     id:"3",
     nom: "maryem",
     prenom: "souyah",
-    dateNaissance: "12/12/1996"
+    dateNaissance: "12/12/1996",
+    statut : "banni"
+
 },
 
 {
     id:"4",
     nom: "sana",
     prenom: "kthiri",
-    dateNaissance: "14/06/1995"
+    dateNaissance: "14/06/1995",
+    statut : "active"
+
 },{
     id:"5",
     nom: "ahmed",
     prenom: "cherif",
-    dateNaissance: "12/03/1996"
+    dateNaissance: "12/03/1996",
+    statut : "active"
+
 }]
+
+export  const addAdherents = function (nom, prenom,dateNaissance,statut) {
+    adherents.push({nom: nom, prenom: prenom , dateNaissance : dateNaissance, statut:statut});
+  console.log('adherent added successfully !!');
+  console.log('-------------------------------------------------');
+}
+ 
+
+export const getAdherent = function (id) { 
+    for(var i = 0; i < adherents.length; i++) {
+      if (adherents[i].id === id) {
+        return Promise.resolve(adherents[i]);
+      }
+    }
+    return null;
+  }
+ // console.log(getAdherent("2"));
 
 
--- src/services/livres.service.js
+++ src/services/livres.service.js
@@ -3,48 +3,92 @@ const livres = [
         id:"1",
         titre: "Le Grand Meaulnes",
         auteur: "Victor Hugo",
-        édition: 2015,
+        edition: 2015,
         NbExemplaires: 20
       }, 
       {
         id:"2",
         titre: "Le Rouge et le Noir",
         auteur: "Victor Del Litto ",
-        édition: 1997,
+        edition: 1997,
         NbExemplaires: 5
       }, 
       {
         id:"3",
         titre: "Le Petit Prince",
         auteur: "Antoine de Saint-Exupéry",
-        édition: 2014,
+        edition: 2014,
         NbExemplaires: 10
       }, 
       {
         id:"4",
         titre: "Lettres de mon moulin",
         auteur: "Alphonse Daudet",
-        édition: 2005,
+        edition: 2005,
         NbExemplaires: 12
       }, 
       {
         id:"5",
         titre: "La Mère",
         auteur: "Pearl Buck",
-        édition: 1971,
+        edition: 1971,
         NbExemplaires: 30
       },
       {
         id:"6",
         titre: "Les Fourmis",
         auteur: "Bernard Werber",
-        édition: 2002,
+        edition: 2002,
         NbExemplaires: 8
       },
        {
         id:"7",
         titre: "Si c'est un homme",
         auteur: "Primo Levi",
-        édition: 2007,
+        edition: 2007,
         NbExemplaires: 6
-      }]
+
+      }
+    ]
+  export const getLivres = function (id) {
+        for(var i = 0; i < livres.length; i++) {
+        if ( livres[i].id === id) {
+          return Promise.resolve(livres[i]);
+        }
+      }
+      return null;
+    }
+console.log(getLivres("1"))  
+
+  export   const setTitre = function (id,titre) {
+      for (let i = 0; i < livres.length; i++) {
+        if(livres[i].id === id){
+          livres[i].titre = titre
+        }
+      }
+      console.log('titre: ', titre)
+    }
+   
+export  const addLivre = function (titre, auteur,edition,NbExemplaires) {
+  livres.push({titre: titre, auteur: auteur ,edition: edition , NbExemplaires : NbExemplaires});
+  console.log('livre added successfully !!');
+  console.log('-------------------------------------------------');
+} 
+ //addLivre('Midnight inking', true);
+
+
+
+ function delay(ms){
+  return new Promise (resolve =>setTimeout(resolve,ms) )
+}
+
+export const fetchLivresById = async(IdLivre) => {
+await delay(2000)
+return livres.find(livre => livre.id === IdLivre)
+}
+
+export const fetchLivres = async searchValue => {
+  await delay(2000)
+  return livres.filter(livre => livre.titre.includes(searchValue))
+} 
+
