8660675fe68ff51c4d38e8f9333e1311b5cff718
ESLEMM <islemkhemiri09@gmail.com>
2020-06-20 02:11:28 +0200

    recherche

--- src/components/livres/PageLivre.js
+++ src/components/livres/PageLivre.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState , useEffect } from "react"
 import {useRouteMatch, useHistory} from 'react-router-dom'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,17 +8,22 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import './Livre.css'
 import ModalBook from '../modalBook/ModalBook'
+import { MDBCol, MDBFormInline, MDBIcon } from "mdbreact";
+import { fetchLivres } from "../../services/livres.service"
 
-function PageAdherents() {
-    
-
+function PageLivre() {    
     var tabLivres = localStorage.getItem("livresTab");
     var listLivres = JSON.parse(tabLivres);
     const history = useHistory()
     let { path } = useRouteMatch()
    const [show,setShow]=useState(false)
-  
+   const [searchValue, setSearchValue] = useState("")
+   const [livres, setLivres] = useState([])
+   const [loading, setLoading] = useState(false)
+
+
   const showModal = () => {
+    console.log("affichier ")
     setShow(true);
   };
 
@@ -27,11 +32,53 @@ function PageAdherents() {
   };
     const test = localStorage.getItem("user")
 
+    useEffect(() => {
+      let didCancel = false
+      const fetchData = async () => {
+        setLoading(true)
+        if (!searchValue) {
+          setLivres([])
+          setLoading(false)
+        } else {
+          const result = await fetchLivres(searchValue)
+          console.log("result: ", didCancel)
+          if (!didCancel) {
+            setLivres(result)
+            setLoading(false)
+          }
+        }
+      }
+      fetchData()
+      return () => {
+        didCancel = true
+      }
+    }, [searchValue])
 
 return (
-    <div className="pageAdherents">
-         
-    <h1>Listes des livres </h1>
+  <div className="pageAdherents">       
+    <h1 className="h1"> Listes des livres </h1>
+    <div>
+    <MDBCol md="4" className="search">
+      <MDBFormInline className="md-form">
+        <MDBIcon icon="search" />
+        <input
+        type="search" 
+        placeholder="Search" 
+        aria-label="Search" 
+        value={searchValue}
+        onChange={e => setSearchValue(e.target.value)}
+        />
+      </MDBFormInline>
+       
+    </MDBCol>
+    <MDBCol md="4" className="add">
+    {test === "bibliothecaire"?(
+         <button md="4" onClick={showModal} > <i className="fa fa-plus" ></i></button> 
+         ):(
+          <button className="btn" >test</button>
+      )}    </MDBCol>
+
+   </div>
 <Paper>
       <Table >
         <TableHead>
@@ -40,25 +87,33 @@ return (
             <TableCell ><b>Auteur</b></TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
+        {loading ? (
+          <div>Loading ... </div>
+        ) : (
+        
+         <TableBody>
           {listLivres.map(n => {    
             return (
               <TableRow key={n.id}>
-                <TableCell >{n.titre}</TableCell>
+                <TableCell > {n.titre}</TableCell>
                 <TableCell numeric>{n.auteur}</TableCell>
                 {test === "bibliothecaire"?(
-                <TableCell> <div><button onClick={showModal}>Ajouter</button> <button>Archiver</button>
+                <TableCell> <div> <button>Archiver</button>
                 <button onClick =  {() =>history.push(`${path}/${n.id}`)} >Details</button></div></TableCell>
                 ):(
                   <TableCell><div><button className="emp">Emprunter</button> 
                   <button onClick ={()=>history.push(`${path}/${n.id}`)} >Details</button></div></TableCell>
-                )}
+                )}                
               </TableRow>
             );
           })}
+            
         </TableBody>
+      
+         )
+        }
       </Table>
-    </Paper>
+    </Paper> 
     <ModalBook show={show} handleClose={hideModal}>
           <p>Modal</p>
           <p>Data</p>
@@ -67,4 +122,4 @@ return (
 )
 }
 
-export default PageAdherents
+export default PageLivre
